4ddff820a1be6b6a92701f83cb97bdd3e327c370
adriana <adrianapenate98@gmail.com>
2020-09-25 15:35:16 +0100

    Refactor - Specific test removed

--- src/test/java/SnakesLaddersShould.java
+++ src/test/java/SnakesLaddersShould.java
@@ -6,14 +6,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SnakesLaddersShould {
 
     @Test
-    public void follow_the_numbers_and_go_to_position_number_three() {
-        int die1 = 1, die2 = 2;
-        String expectedOutput = "Player 1 is on square 3";
-
-        assertThat(SnakesLadders.play(die1, die2)).isEqualTo(expectedOutput);
-    }
-
-    @Test
     public void follow_the_numbers_and_go_to_position_number_four() {
         int die1 = 2, die2 = 2;
         String expectedOutput = "Player 1 is on square 4";
