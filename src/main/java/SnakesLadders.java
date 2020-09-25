b7e5ff763cb66b206e9520f7997cb4bf5667b4ed
adriana <adrianapenate98@gmail.com>
2020-09-25 15:34:00 +0100

    Green test - follow_the_numbers_and_go_to_position_number_four

--- src/main/java/SnakesLadders.java
+++ src/main/java/SnakesLadders.java
@@ -1,6 +1,6 @@
 public class SnakesLadders {
 
     public static String play(int die1, int die2) {
-        return "Player 1 is on square 3";
+        return "Player 1 is on square " + (die1 + die2);
     }
 }
