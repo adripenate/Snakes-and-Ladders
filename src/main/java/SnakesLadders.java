74b93cce9e454177ced3b4d289848f9a38e48bca
adriana <adrianapenate98@gmail.com>
2020-09-25 15:47:56 +0100

    Green test - remember_last_position

--- src/main/java/SnakesLadders.java
+++ src/main/java/SnakesLadders.java
@@ -1,6 +1,8 @@
 public class SnakesLadders {
+    int position = 0;
 
-    public static String play(int die1, int die2) {
-        return "Player 1 is on square " + (die1 + die2);
+    public String play(int die1, int die2) {
+        this.position += die1+die2;
+        return "Player 1 is on square " + position;
     }
 }
--- src/test/java/SnakesLaddersShould.java
+++ src/test/java/SnakesLaddersShould.java
@@ -9,7 +9,24 @@ public class SnakesLaddersShould {
     public void follow_the_numbers_and_go_to_position_number_four() {
         int die1 = 2, die2 = 2;
         String expectedOutput = "Player 1 is on square 4";
+        SnakesLadders game = new SnakesLadders();
 
-        assertThat(SnakesLadders.play(die1, die2)).isEqualTo(expectedOutput);
+        assertThat(game.play(die1, die2)).isEqualTo(expectedOutput);
     }
+
+    @Test
+    public void remember_last_position() {
+        int die1 = 1, die2 = 2;
+        String expectedOutput = "Player 1 is on square 3";
+        SnakesLadders game = new SnakesLadders();
+
+        assertThat(game.play(die1, die2)).isEqualTo(expectedOutput);
+
+        die1 = 1;
+        die2 = 1;
+        expectedOutput = "Player 1 is on square 5";
+
+        assertThat(game.play(die1, die2)).isEqualTo(expectedOutput);
+    }
+
 }
