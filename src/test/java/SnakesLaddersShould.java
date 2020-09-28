ead53cc7608d3c0aa28823b2b32c03d89e1e1edc
adriana <adrianapenate98@gmail.com>
2020-09-28 09:31:28 +0100

    Red test - make_player_have_another_go_when_dies_are_equal

--- src/test/java/SnakesLaddersShould.java
+++ src/test/java/SnakesLaddersShould.java
@@ -46,4 +46,19 @@ public class SnakesLaddersShould {
         assertThat(game.play(die1, die2)).isEqualTo(expectedOutput);
     }
 
+    @Test
+    public void make_player_have_another_go_when_dies_are_equal() {
+        int die1 = 2, die2 = 2;
+        String expectedOutput = "Player 1 is on square 4";
+        SnakesLadders game = new SnakesLadders();
+
+        assertThat(game.play(die1, die2)).isEqualTo(expectedOutput);
+
+        die1 = 1;
+        die2 = 1;
+        expectedOutput = "Player 1 is on square 6";
+
+        assertThat(game.play(die1, die2)).isEqualTo(expectedOutput);
+    }
+
 }
