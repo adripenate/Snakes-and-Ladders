ab673838e2ab4edbbe4b472c322321bafdd908c7
adriana <adrianapenate98@gmail.com>
2020-09-28 09:25:15 +0100

    Refactor - Duplicated code deleted

--- src/main/java/SnakesLadders.java
+++ src/main/java/SnakesLadders.java
@@ -1,21 +1,21 @@
 public class SnakesLadders {
     Player player1, player2;
-    boolean player1Turn = true;
+    Player playerWithTurn;
 
     public SnakesLadders() {
         this.player1 = new Player(1);
         this.player2 = new Player(2);
+        this.playerWithTurn = player1;
     }
 
     public String play(int die1, int die2) {
-        if(player1Turn){
-            player1.advance(die1, die2);
-            player1Turn = false;
-            return player1.keepPlaying();
+        playerWithTurn.advance(die1, die2);
+        String playerState = playerWithTurn.keepPlaying();
+        if(playerWithTurn == player1){
+            playerWithTurn = player2;
         }else{
-            player2.advance(die1, die2);
-            player1Turn = true;
-            return player2.keepPlaying();
+            playerWithTurn = player1;
         }
+        return playerState;
     }
 }
