2f5642b7d0abec03f66a589c8e58cd07382a1755
adriana <adrianapenate98@gmail.com>
2020-09-28 11:03:41 +0100

    All snakes squares added to hashmap

--- src/main/java/SnakesLadders.java
+++ src/main/java/SnakesLadders.java
@@ -23,6 +23,14 @@ public class SnakesLadders {
     {{
         put(16, 6);
         put(46, 25);
+        put(49, 11);
+        put(62, 19);
+        put(64, 60);
+        put(74, 53);
+        put(89, 68);
+        put(92, 88);
+        put(95, 75);
+        put(99, 80);
     }};
 
     public SnakesLadders() {
