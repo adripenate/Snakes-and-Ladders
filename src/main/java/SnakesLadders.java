e01e1a31bcbdbcd9455fc8103a6550a0ae473715
adriana <adrianapenate98@gmail.com>
2020-09-29 11:07:14 +0100

    Green test - show_game_over_message_when_another_player_has_won

--- src/main/java/SnakesLadders.java
+++ src/main/java/SnakesLadders.java
@@ -1,18 +1,22 @@
 public class SnakesLadders {
     private Player player1, player2;
     private Player playerWithTurn;
+    private boolean gameOver;
 
     public SnakesLadders() {
         this.player1 = new Player(1, 100);
         this.player2 = new Player(2, 100);
         this.playerWithTurn = player1;
+        this.gameOver = false;
     }
 
     public String play(int die1, int die2) {
+        if (gameOver) return "Game over!";
         playerWithTurn.advance(die1, die2);
         if(playerIsInLadderSquare()) playerWithTurn.advanceTo(getLadderEndSquare());
         if (playerIsInSnakeSquare()) playerWithTurn.advanceTo(getSnakeEndSquare());
         String playerState = playerWithTurn.keepPlaying();
+        if(getActualSquare() == 100) gameOver = true;
         playerWithTurn = whoseTurnIsIt(die1, die2) ? player1 : player2;
         return playerState;
     }
